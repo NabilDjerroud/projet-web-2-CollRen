de90fa6bad81f48bab9cb4cd13eb025af57b8cdc
René de Montigny <e2395944@cmaisonneuve.qc.ca>
2024-07-08 12:11:37 -0400

    Rendre searchBar fonctionnelle dans console
    Pour l'instant, il faut ouvrir la console pour voir les
    résultats.

--- frontend/src/components/site/BarreRecherche/BarreRecherche.js
+++ frontend/src/components/site/BarreRecherche/BarreRecherche.js
@@ -6,50 +6,139 @@ import ChampText from '../../partialsFormulaire/ChampText/ChampText.js';
 import './Recherche.css'
 
 function BarreRecherche({ t }) {
+    var results = {};
+    results.positif = []
+    results.negatif = []
 
-    // Obtenir les données de la table voitures
-    async function getVoituresData() {
+    let arrayVoitures = [];
 
-        const data = {
-            method: "GET",
-            headers: {
-                "Content-Type": "application/json"
+    const [arrayResultatRecherche, setArrayResultatRecherche] = useState([]);
+
+    async function setDataVoitures() {
+
+        const fetchVoitures = async () => {
+            try {
+                const response = await fetch(`${t("fetch")}voitures`);
+                if (!response.ok) {
+                    throw new Error(`HTTP error! status: ${response.status}`);
+                }
+                arrayVoitures = await response.json();
+
+            } catch (error) {
+                console.error('Error fetching voitures:', error);
             }
         };
+        fetchVoitures();
+    };
+    setDataVoitures();
 
-        const response = await fetch(`${t("fetch")}voitures`, data);
-       console.log(response);
-        return data
+    function trimString(s) {
+        var l = 0, r = s.length - 1;
+        while (l < s.length && s[l] == ' ') l++;
+        while (r > l && s[r] == ' ') r -= 1;
+        return s.substring(l, r + 1);
     }
 
-/*     const [dataVoitures, setDataVoitures] = useState(getVoituresData());
-    console.log(dataVoitures); */
+    function compareObjects(o1, o2) {
+        var k = '';
+        for (k in o1) if (o1[k] != o2[k]) return false;
+        for (k in o2) if (o1[k] != o2[k]) return false;
+        return true;
+    }
 
-    // console.log(dataVoitures);
+    function itemExists(haystack, needle) {
+
+        for (var i = 0; i < haystack.length; i++) if (compareObjects(haystack[i], needle)) return true;
+        return false;
+    }
+
+    function searchFor(ArrOfObjects, toSearch, objetPrincipal) {
+
+        toSearch = trimString(toSearch).toLowerCase(); // trim & lower case it
+        ArrOfObjects.map((objects) => {
 
 
-    // Enregistrer les données dans 2 variables
+            for (var i = 0; i < objects.length; i++) {
+                for (var value in objects[i]) {
+                    if (objects[i][value].toLowerCase().indexOf(toSearch) != -1) {
+
+                        if (!itemExists(results, objects[i])) {
+                            // Si ce résultat n'est pas déjà là, ajoute-le
+                            results.positif.indexOf(objetPrincipal) === -1 ? results.positif.push(objetPrincipal) : console.log("This item already exists")
+                        } else {
+                            results.negatif.push(objetPrincipal);
+
+                        }
+                    }
+                }
+            }
+        })
+
+        return results;
+    }
 
 
-    // onChange, enlever d'une des variables ce qui ne coresspond pas (fonction js --> include)
 
 
+    const handleInputChange = async (e) => {
+        e.preventDefault();
+        let termeRecherche = e.target[0].value;
+        console.log(termeRecherche);
+        if (termeRecherche == []) return []
+        let ObjetContientRecherche;
+        let arrayOfResults = [];
+
+
+        for (let i = 0; i < arrayVoitures.length; i++) {
+            const elementCarburant = [JSON.parse(arrayVoitures[i].carburant.type)];
+            let elementCorp = [JSON.parse(arrayVoitures[i].corp.type)];
+            const elementModele = [JSON.parse(arrayVoitures[i].modele.type)];
+            const elementMotopropulseur = [JSON.parse(arrayVoitures[i].motopropulseur.type)];
+            const elementTransmission = [JSON.parse(arrayVoitures[i].transmission.type)];
+
+            const arrayOfElementToSearchIn = [elementCarburant, elementModele, elementMotopropulseur, elementTransmission]
+
+            // filterIfIsThere(elementCarburant, termeRecherche)  ||
+            /*filterIfIsThere(elementCorp, termeRecherche) ||
+            filterIfIsThere(elementTransmission, termeRecherche) ||
+            filterIfIsThere(elementMotopropulseur, termeRecherche) ||
+            filterIfIsThere(elementModele, termeRecherche) */
+            ObjetContientRecherche = searchFor(arrayOfElementToSearchIn, termeRecherche, arrayVoitures[i])
+
+
+
+
+        }
+
+        if (ObjetContientRecherche.positif !== undefined) {
+            console.log(ObjetContientRecherche.positif)
+        } else {
+            console.log('undefined')
+        }
+
+
+
+
+        setArrayResultatRecherche(arrayOfResults);
+
+
+        return arrayResultatRecherche;
+    };
 
     // Faire apparaître les résultats sous la barre de recherche après l'entré de 2 caratères minimum
-    const handleRechercheWithShow = true;
 
 
     return (
-  
-            <div className="form-container">
-                <h1 className="text-4xl font-titre font-bold">{t("barreRecheche.titre")}</h1>
-                <form className='form-inner' onChange={handleRechercheWithShow}>
-                    <ChampText type="text" name="termeRecherche" placeholder={t("barreRecheche.placeHolder")} />
-                    <div className="flex justify-center">
-                        <Bouton>Recherche</Bouton>
-                    </div>
-                </form>
-            </div>
+
+        <div className="form-container">
+            <h1 className="text-4xl font-titre font-bold">{t("barreRecheche.titre")}</h1>
+            <form onSubmit={handleInputChange} className='form-inner'>
+                <ChampText type="text" name="termeRecherche" placeholder={t("barreRecheche.placeHolder")} />
+                <div className="flex justify-center">
+                    <Bouton type="submit">{t("barreRecheche.titre")}</Bouton>
+                </div>
+            </form>
+        </div>
 
 
     );
